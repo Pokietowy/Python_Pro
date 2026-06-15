14dc93f4596074533f000c802b8dd1b20ca83398
Pokietowy <pokietowy@gmail.com>
2026-06-15 17:00:48 +0200

    Update projekt3.py

--- projekt3.py
+++ projekt3.py
@@ -1,1 +1,30 @@
+from flask import Flask
+import random
 
+app = Flask("test")
+
+@app.route("/")
+def hello_world():
+    return '''
+        <h1>Witaj świecie!</h1>
+        <a href="/random">Wyświetl losowy fakt!</a><br/>
+        <a href="/generatorpass">Wygeneruj losowe hasło!</a>
+    '''
+
+@app.route("/random")
+def random_fact():
+    facts_list = ["Test", "Test2"]
+    return f'<p>{random.choice(facts_list)}</p>'
+
+@app.route("/generatorpass")
+def gen_pass():
+    pass_length = 12
+    elements = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+
+app.run(debug=True)
